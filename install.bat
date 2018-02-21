cf9dac88defbc092493702b0788103548e410e45
KeenRivals <KeenRivals@users.noreply.github.com>
2018-02-21 09:44:43 -0500

    Initial commit Windows 10 1709. Hides the following: * Search button * Task View button * People button

---
+++ install.bat
@@ -0,0 +1,17 @@
+:: Default Taskbar Settings
+:: Apply some defaults to the appearance of the user taskbar.
+
+:: Load the default user's registry hive.
+reg load HKLM\DefaultUsers C:\Users\Default\ntuser.dat
+
+:: Hide the taskbar search button by default.
+reg add HKLM\DefaultUsers\Software\Microsoft\Windows\CurrentVersion\Search /v SearchboxTaskbarMode /t REG_DWORD /d 0
+
+:: Hide the Task View button by default.
+reg add HKLM\DefaultUsers\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v ShowTaskViewButton /t REG_DWORD /d 0
+
+:: Hide the People button by default
+reg add HKLM\DefaultUsers\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People /v PeopleBand /t REG_DWORD /d 0
+
+:: Unload the default user's hive
+reg unload HKLM\DefaultUsers
