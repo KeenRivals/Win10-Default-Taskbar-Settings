6070ed8c66541cf57c9a43590dfcc5d106820320
KeenRivals <KeenRivals@users.noreply.github.com>
2021-06-08 14:38:40 -0400

    Add /f parameter to force changes

--- install.bat
+++ install.bat
@@ -5,16 +5,16 @@
 reg load HKLM\DefaultUsers C:\Users\Default\ntuser.dat
 
 :: Hide the taskbar search button by default.
-reg add HKLM\DefaultUsers\Software\Microsoft\Windows\CurrentVersion\Search /v SearchboxTaskbarMode /t REG_DWORD /d 0
+reg add HKLM\DefaultUsers\Software\Microsoft\Windows\CurrentVersion\Search /v SearchboxTaskbarMode /t REG_DWORD /d 0 /f
 
 :: Hide the Task View button by default.
-reg add HKLM\DefaultUsers\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v ShowTaskViewButton /t REG_DWORD /d 0
+reg add HKLM\DefaultUsers\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v ShowTaskViewButton /t REG_DWORD /d 0 /f
 
 :: Hide the People button by default
-reg add HKLM\DefaultUsers\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People /v PeopleBand /t REG_DWORD /d 0
+reg add HKLM\DefaultUsers\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People /v PeopleBand /t REG_DWORD /d 0 /f
 
 :: Hide the News Feed by default
-reg add HKLM\DefaultUsers\Software\Microsoft\Windows\CurrentVersion\Feeds /v ShellFeedsTaskbarViewMode /t REG_DWORD /d 2
+reg add HKLM\DefaultUsers\Software\Microsoft\Windows\CurrentVersion\Feeds /v ShellFeedsTaskbarViewMode /t REG_DWORD /d 2 /f
 
 :: Unload the default user's hive
 reg unload HKLM\DefaultUsers
